9114e805f9744baba225cff70d31fc0c765493cf
Denis Maley <denis2digifit.eu>
2020-02-11 11:17:14 +0100

    solution for problem 2

---
+++ app/main/lib/solutions/2/solution.py
@@ -0,0 +1,15 @@
+even_fib = [0, 2]
+
+
+def next_even_fib():
+    return 4 * even_fib[-1] + even_fib[-2]
+
+
+# TODO After migrating to Python 3.8 simplify the loop
+# using assignment expressions
+# https://www.python.org/dev/peps/pep-0572/#sysconfig-py
+# https://stackoverflow.com/questions/50297704/syntax-and-assignment-expressions-what-and-why
+def sum_even_fib(limit):
+    while next_even_fib() < limit:
+        even_fib.append(next_even_fib())
+    return sum(even_fib)
